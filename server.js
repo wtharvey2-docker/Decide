815f26d0f6d1b09edc83de4a4cbd6e6662c32745
wtharvey2 <55966891+wtharvey2@users.noreply.github.com>
2021-01-02 13:29:19 -0800

    an attempt to allow heroku to get the right port

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ router.get('/decideQuestion',function(req,res){
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port);
 
 console.log('Running at Port 3000 or ' + String(process.env.port));
